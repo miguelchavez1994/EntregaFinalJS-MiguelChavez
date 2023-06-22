699ae9cbeacddcb37a8c02ad2cc4aa3b6bdad8ca
Miguel Chavez <miguelchavez1994@gmail.com>
2023-06-22 12:47:33 -0500

    v4

--- js/renderPerritos.js
+++ js/renderPerritos.js
@@ -7,18 +7,26 @@ function renderPerritos() {
 
     if (perritosPerdidos.length > 0) {
         perritosPerdidos.forEach(perrito => {
-            contenido += `<div class="col-md-3 mb-5">
-            <a href="ver-producto.html" onclick="verProducto(${perrito.id})" class="text-decoration-none">
-                <div class="card text-center border border-0">
-                    <img src="${perrito.imagen}" class="card-img-top" alt="${perrito.nombre}">
-                    <div class="card-body">
-                        <p class="card-text text-primary"><b>$${perrito.precio}</b></p>
-                        <p class="text-secondary">${perrito.nombre}</p>
-                    </div>
-                </div>
-            </a>
-          </div>`;
+            contenido += `<div class="contenedor col-md-5 mb-5">
+                            <a href="ver-producto.html" onclick="verProducto(${perrito.id})" class="text-decoration-none">
+                                <div class="card text-center border border-0">
+                                    <img src="${perrito.imagen}" class="card-img-top" alt="${perrito.nombre}">
+                                    <div class="card-body">
+                                        <p class="perritoNombre card-text text-primary">Mi nombre es ${perrito.nombre}</p>
+                                        <p class="text-secondary"><b>Me encontraron en ${perrito.distrito}</b></p>
+                                        <p class="text-secondary"><b>Me encontraron el día ${perrito.fechaEncontrado}</b></p>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>`;
         });
+        /*
+        <a es todo el link que contiende el ID de la tarjeta
+            <div es la tarjeta 
+                <img es la imagen que le otorgare perrito.imagen
+                <div es otro div que tendra la informacion de cada tarjeta
+                    < la info de cada tarjeta 
+        */
     } else {
         contenido += `<div class="alert alert-danger text-center" role="alert">No se encontraron productos por el término de búsqueda!</div>`;
     }
