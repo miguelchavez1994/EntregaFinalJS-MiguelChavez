40da8c34e107944973f59630bfd09fb0bf030f26
Miguel Chavez <miguelchavez1994@gmail.com>
2023-07-15 13:28:56 -0500

    evfv

--- js/toastify.js
+++ js/toastify.js
@@ -1,15 +1,17 @@
-const validarFormulario = () => {
+const validarFormulario = (event) => {
+    event.preventDefault(); // Prevent form submission
+
     let nombre = document.getElementById("nombre");
     let apellido = document.getElementById("apellidos");
     let email = document.getElementById("email");
     let telefono = document.getElementById("telefono");
     let fechaNacimiento = document.getElementById("fechaNacimiento");
-    let genero = document.getElementById("genero");
+    let genero = document.querySelector('input[name="genero"]:checked');
     let direccion = document.getElementById("direccion");
     let pais = document.getElementById("pais");
     let ciudad = document.getElementById("ciudad");
 
-
+    
 
     if (nombre.value == "") {
         mostrarMensaje("Complete el campo Nombre!", "error");
@@ -57,12 +59,10 @@ const validarFormulario = () => {
         ciudad.focus();
         return false;
     } 
-     
 
-    
     mostrarMensaje("WOOF! Registro exitoso. Te contactaremos para el proceso de adopción.", "ok");
-    guardarDatos(nombre.value, apellido.value, email.value, telefono.value, fechaNacimiento.value, genero.value, direccion.value, pais.value,ciudad.value);
-}
+    guardarDatos(nombre.value, apellido.value, email.value, telefono.value, fechaNacimiento.value, genero.value, direccion.value, pais.value, ciudad.value);
+};
 
 const mostrarMensaje = (texto, tipo) => {
     let colorFondo = (tipo == "error") ? "#FF0000" : "#00FF00";
@@ -70,18 +70,22 @@ const mostrarMensaje = (texto, tipo) => {
     Toastify({
         text: texto,
         duration: 2000,
-        gravity: "top", // `top` or `bottom`
-        position: "right", // `left`, `center` or `right`
+        gravity: "top", 
+        position: "center", 
         style: {
             color: 'white',
             background: colorFondo,
         },
     }).showToast();
-}
+};
 
-const guardarDatos = (nombre, apellido, email, telefono, fechaNacimiento, genero, direccion, pais, ciudad ) => {
-    const datosUsuario = {nombre:nombre, apellido:apellido,email:email, telefono:telefono,fechaNacimiento:fechaNacimiento,genero:genero, direccion:direccion, pais:pais, ciudad:ciudad  }
+const guardarDatos = (nombre, apellido, email, telefono, fechaNacimiento, genero, direccion, pais, ciudad) => {
+    const datosUsuario = { nombre, apellido, email, telefono, fechaNacimiento, genero, direccion, pais, ciudad };
     localStorage.setItem("texto", JSON.stringify(datosUsuario));
-}
+
+    
+    const storedData = JSON.parse(localStorage.getItem("texto"));
+    console.log(storedData);
+};
 
 document.getElementById("submit-boton").onclick = validarFormulario;
