f08e07e9e05661c5536bd6378653592e1359be42
Miguel Chavez <miguelchavez1994@gmail.com>
2023-07-26 16:00:40 -0500

    feroifm

--- js/perritos.js
+++ js/perritos.js
@@ -1,8 +1,8 @@
 const perritosPerdidos = [
     {id:1 , raza: "FRENCH BULLDOG", tamano:"MEDIANO" , color: "NEGRO" , sexo: "HEMGRA" , nombre: "TOBI" , ciudad:"LIMA" , distrito:"SAN MIGUEL" , fechaEncontrado:"19/06/2023" , imagen:`./multimedia/images/french-bulldog.jpg`},
-    {id:2 ,raza: "PITBULL", tamano:"GRANDE" , color: "BLANCO" , sexo: "MACHO", nombre: "CUKI" , ciudad:"LIMA" , distrito:"PUEBLO LIBRE" , fechaEncontrado:"09/05/2023" , imagen: `../multimedia/images/pitbull.jpg`},
-    {id:3 ,raza: "LABRADOR", tamano:"GRANDE" , color: "DORADO" , sexo: "MACHO" , nombre: "BOBY" , ciudad:"LIMA" , distrito:"MAGDALENA" , fechaEncontrado:"15/06/2023" , imagen: `../multimedia/images/labrador.png`},
-    {id:4 ,raza: "COCKER", tamano:"MEDIANO" , color: "CARAMELO" , sexo: "HEMGRA" , nombre: "LUCA" , ciudad:"LIMA" , distrito:"MIRAFLORES" , fechaEncontrado:"10/06/2023" , imagen: `../multimedia/images/cocker.jpg`},
-    {id:5 ,raza: "JACK RUSSELL", tamano:"PEQUEÑO" , color: "BLANCO" , sexo: "MACHO" , nombre: "LIONEL" , ciudad:"LIMA" , distrito:"SAN MIGUEL" , fechaEncontrado:"30/05/2023" , imagen: `../multimedia/images/jack-russell.jpg`},
-    {id:6 ,raza: "MESTIZO", tamano:"MEDIANO" , color: "MARRON" , sexo: "MACHO" , nombre: "LARRY" , ciudad:"LIMA" , distrito:"MAGDALENA" , fechaEncontrado:"03/06/2023" , imagen: `../multimedia/images/mestizo.jpg`},
+    {id:2 ,raza: "PITBULL", tamano:"GRANDE" , color: "BLANCO" , sexo: "MACHO", nombre: "CUKI" , ciudad:"LIMA" , distrito:"PUEBLO LIBRE" , fechaEncontrado:"09/05/2023" , imagen: `./multimedia/images/pitbull.jpg`},
+    {id:3 ,raza: "LABRADOR", tamano:"GRANDE" , color: "DORADO" , sexo: "MACHO" , nombre: "BOBY" , ciudad:"LIMA" , distrito:"MAGDALENA" , fechaEncontrado:"15/06/2023" , imagen: `./multimedia/images/labrador.png`},
+    {id:4 ,raza: "COCKER", tamano:"MEDIANO" , color: "CARAMELO" , sexo: "HEMGRA" , nombre: "LUCA" , ciudad:"LIMA" , distrito:"MIRAFLORES" , fechaEncontrado:"10/06/2023" , imagen: `./multimedia/images/cocker.jpg`},
+    {id:5 ,raza: "JACK RUSSELL", tamano:"PEQUEÑO" , color: "BLANCO" , sexo: "MACHO" , nombre: "LIONEL" , ciudad:"LIMA" , distrito:"SAN MIGUEL" , fechaEncontrado:"30/05/2023" , imagen: `./multimedia/images/jack-russell.jpg`},
+    {id:6 ,raza: "MESTIZO", tamano:"MEDIANO" , color: "MARRON" , sexo: "MACHO" , nombre: "LARRY" , ciudad:"LIMA" , distrito:"MAGDALENA" , fechaEncontrado:"03/06/2023" , imagen: `./multimedia/images/mestizo.jpg`},
 ];
--- js/renderPerritos.js
+++ js/renderPerritos.js
@@ -18,7 +18,7 @@ function renderPerritos() {
     if (perritosPerdidos.length > 0) {
         perritosPerdidos.forEach(perrito => {
             contenido += `<div class="contenedor col-md-4 col-sm-12 mb-3 justify-content-center">
-                            <a href="/datos-patita.html" onclick="verPerrito(${perrito.id})" class="text-decoration-none">
+                            <a href="./datos-patita.html" onclick="verPerrito(${perrito.id})" class="text-decoration-none">
                                 <div class="tarjeta card text-center border border-0">
                                     <img src="${perrito.imagen}" class="card-img-top" alt="${perrito.nombre}">
                                     <div class="patita-datos card-body">
@@ -30,13 +30,7 @@ function renderPerritos() {
                             </a>
                         </div>`;
         });
-        /*
-        <a es todo el link que contiende el ID de la tarjeta
-            <div es la tarjeta 
-                <img es la imagen que le otorgare perrito.imagen
-                <div es otro div que tendra la informacion de cada tarjeta
-                    < la info de cada tarjeta 
-        */
+        
     } else {
         contenido += `<div class="alert alert-danger text-center" role="alert">No se encontraron productos por el término de búsqueda!</div>`;
     }
@@ -49,4 +43,4 @@ pedirPerritos().then(perritosPerdidos => {
     renderPerritos(perritosPerdidos);
 });
 
-//renderPerritos();
+
